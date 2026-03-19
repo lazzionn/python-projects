3da3bf4284c675d37a7adb8c59cf3a6909c0a2b7
lazzionn <lazarotoon@gmail.com>
2026-03-19 09:27:58 -0300

    Jogo_da_forca_V1.py
    Minigame da forca desenvolvido em Python do zero.
    
    - 50 palavras aleatórias divididas em 5 categorias
    - Limite de 8 erros por partida
    - Palavra se revelando letra por letra
    - Contador de jogadas, erros e acertos
    - Opção de reiniciar ou sair ao fim de cada partida
    - Verificação de entradas inválidas (números, palavras erradas)
    
    Desenvolvido com: Python 3
    
    by lazzionn

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,4 +1,4 @@
-# jogo da forca feito por lazaro
+# jogo da forca feito por lazzionn
 import random
 import os
 
@@ -7,31 +7,36 @@ acertos = 0
 jogadas = 0
 resposta_final = ''
 acerto_direto = 0
-
-#                 #0123456
 r2 = ''
 limite_erros = 8
-base_palavras = ['cachorro', 'bicicleta', 'picole', 'maça', 'computador', 'fazenda', 'borboleta', 'trabalho', 'escola', 'carro', 'motor']
+base_palavras = [
+    # animais
+    'cachorro', 'borboleta', 'elefante', 'jacare', 'papagaio',
+    'tartaruga', 'hamster', 'camaleao', 'tubarao', 'pinguim',
+    
+    # tecnologia
+    'computador', 'teclado', 'monitor', 'impressora', 'notebook',
+    'algoritmo', 'programa', 'internet', 'servidor', 'software',
+    
+    # objetos
+    'bicicleta', 'cadeira', 'geladeira', 'televisao', 'microfone',
+    'mochila', 'guarda-chuva', 'calendario', 'calculadora', 'lanterna',
+    
+    # natureza
+    'fazenda', 'cachoeira', 'montanha', 'floresta', 'vulcao',
+    'tempestade', 'arco-iris', 'deserto', 'oceano', 'peninsula',
+    
+    # cotidiano
+    'trabalho', 'escola', 'mercado', 'restaurante', 'hospital',
+    'biblioteca', 'academia', 'farmacia', 'padaria', 'aeroporto'
+]
 palavra_secreta = random.choice(base_palavras)
 while resposta_final != palavra_secreta:
     
     r1 = input("Digite uma letra ou a palavra secreta: ")
     
     # pra verificar entrada
-    if erros > (limite_erros - 1):
-        os.system('clear')
-        print("Acabou suas tentativas! ")
-        sair = input("Deseja recomeçar? [sim]/[nao] ").lower().startswith("n")
-        if sair is True:
-            break
-        else:
-            resposta_final = ''
-            r2 = ''
-            erros = 0
-            acertos = 0
-            jogadas = 0
-            palavra_secreta = random.choice(base_palavras)
-            continue
+    
     
     if len(r1) <= 0:
         print("Digite no minimo uma letra!")
@@ -40,11 +45,8 @@ while resposta_final != palavra_secreta:
     if len(r1) > 1 and r1 != palavra_secreta:
         os.system('clear')
         print("Você errou a palavra secreta")
-        sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("n")
-                
+        sair = input("Deseja recomeçar o jogo? [sim]/[nao] ").lower().startswith("s")
         if sair is True:
-            break
-        else:
             resposta_final = ''
             r2 = ''
             erros = 0
@@ -52,23 +54,24 @@ while resposta_final != palavra_secreta:
             jogadas = 0
             palavra_secreta = random.choice(base_palavras)
             continue
+        else:
+            break
         
     if r1.isdigit():
         os.system('clear')
         print("Por favor, digite apenas letras!")
         
-        sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("n")
-                
+        sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("s")
         if sair is True:
-            break
-        else:
             resposta_final = ''
             r2 = ''
             erros = 0
             acertos = 0
             jogadas = 0
             palavra_secreta = random.choice(base_palavras)
-            continue 
+            continue
+        else:
+            break
         
 
     ###############
@@ -76,21 +79,21 @@ while resposta_final != palavra_secreta:
     if r1 == palavra_secreta:
         os.system('clear')
         acerto_direto += 1
-        print(f"Parabens!  Você acertou a palavra secreta, com o total de {jogadas} jogadas, {erros} erros e {acerto_direto} acerto direto!\nA palavra era {palavra_secreta.upper()}")
-        sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("n")
+        print(f"Caramba! Você acertou a palavra secreta completa com \n{jogadas} jogadas e {erros} erro(s)!\nA palavra era {palavra_secreta.upper()}")
+        sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("s")
         if sair is True:
-            break
-        else:
             resposta_final = ''
-            r1 = ''
             r2 = ''
             erros = 0
             acertos = 0
             jogadas = 0
             palavra_secreta = random.choice(base_palavras)
             continue
+        else:
+            break
     else:
         if r1 in palavra_secreta: 
+            os.system('clear')
             print(f"'{r1}' está na palavra secreta!") 
             jogadas += 1
             acertos += 1
@@ -103,16 +106,13 @@ while resposta_final != palavra_secreta:
                 if letra in r2: 
                     resposta_final += letra 
                 else:
-                    resposta_final += '*' 
+                    resposta_final += '_ ' 
             print(f'Palavra formada: {resposta_final}')
             if resposta_final == palavra_secreta:
                 os.system('clear')
                 print(f"Parabens!  Você acertou a palavra secreta, com o total de {jogadas} jogadas, {erros} erros e {acertos} acertos!")
-                sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("n")
-                
+                sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("s")
                 if sair is True:
-                    break
-                else:
                     resposta_final = ''
                     r2 = ''
                     erros = 0
@@ -120,13 +120,30 @@ while resposta_final != palavra_secreta:
                     jogadas = 0
                     palavra_secreta = random.choice(base_palavras)
                     continue
+                else:
+                    break
         else: 
+            os.system('clear')
             print(f"'{r1}' não está na palavra secreta!")
+            print(f'Palavra formada: {resposta_final}')
             jogadas += 1
             erros += 1
             print(f"Tentativas: {jogadas}")
             print(f"Erros: [{erros}/{limite_erros}]")
+            if erros > (limite_erros - 1):
+                os.system('clear')
+                print(f"Acabou suas tentativas! \nA palavra era {palavra_secreta.upper()}")
+                sair = input("Deseja continuar? [sim]/[nao] ").lower().startswith("s")
+                if sair is True:
+                    resposta_final = ''
+                    r2 = ''
+                    erros = 0
+                    acertos = 0
+                    jogadas = 0
+                    palavra_secreta = random.choice(base_palavras)
+                    continue
+                else:
+                    break
             
-            continue
 
-print("\nAté mais! \n\n\nby: Lazz")
+print("\nAté mais! \n\n\nby: lazzion")
