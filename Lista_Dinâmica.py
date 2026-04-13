f9217d910668357824d0a2fe885babdaa1599da3
Lázaro Santos <lazarotoon@gmail.com>
2026-04-13 14:14:13 -0300

    Update Lista_Dinâmica.py
    projeto finalizado, adicionei uma estrutura melhor pra visualização dos itens da lista, e removi a antiga que aparecia quando adicionava um novo item na lista

--- Lista_Dinâmica.py
+++ Lista_Dinâmica.py
@@ -1,4 +1,4 @@
-# AINDA EM CONSTRUÇÃO!!! PUBLIQUEI SÓ PRA DEIXAR SALVO AQUI
+# feito por lazzion
 lista_compras = ['banana', 'maçã', 'ovos']
 
 adicionar = ''
@@ -15,20 +15,23 @@ while True:
         if r1 == '1':
             adicionar = input("Digite oque você deseja adicionar á lista: ")
             lista_compras.append(adicionar)
-            for item in lista_compras:
-                print(f"{i}. {item}")
-                i += 1
         elif r1 == '2':
             delete = input("Qual o número do item que você deseja remover? (Apenas número, por favor)").isdigit()
             del lista_compras[delete]
             continue
         elif r1 == '3':
-            for item in lista_compras:
-                print(f"\t {i}. {item.upper()}")
-                i += 1
+            print("\n" + "=" * 30)
+            print("     🛒 LISTA DE COMPRAS")
+            print("=" * 30)
+        
+            if not lista_compras:
+                print("Sua lista está vazia.")
+            else:
+                for i, item in enumerate(lista_compras, start=1):
+                    print(f"{i:02d} | {item.capitalize()}")
+                    print("=" * 30 + "\n")
         else:
             break
     else:
         print("Escolha apenas uma das opções!")
         continue
-    
