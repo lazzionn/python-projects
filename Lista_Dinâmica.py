7a759f8235dfaa68574f26219cfd73ba287fc124
Lázaro Santos <lazarotoon@gmail.com>
2026-04-14 09:17:51 -0300

    Update Lista_Dinâmica.py
    Removi os valores padrão que vinham na lista, deixei a execução do código mais agradável visualmente com clear e deixei a opção de trocar o nome da lista também

--- Lista_Dinâmica.py
+++ Lista_Dinâmica.py
@@ -1,37 +1,49 @@
-# feito por lazzion
-lista_compras = ['banana', 'maçã', 'ovos']
+#made by lazzion
+import os
+lista_compras = []
 
 adicionar = ''
 delete = ''
 i = 1
-opcoes = ['1', '2', '3', '4']
+opcoes = ['1', '2', '3', '4', '5']
+nome_lista = '🛒 LISTA DE COMPRAS'
 while True:
 
-    print("Oque gostaria de fazer? \n [1] Adicionar um item à lista \n [2] Excluir um item da lista \n [3] Listar valores \n [4] Sair")
+    print("Oque gostaria de fazer? \n [1] Adicionar um item à lista \n [2] Excluir um item da lista \n [3] Listar valores \n [4] Renomear lista \n [5] Sair")
     r1 = input()
     i = 1
     
     if r1 in opcoes:
         if r1 == '1':
+            os.system('clear')
             adicionar = input("Digite oque você deseja adicionar á lista: ")
             lista_compras.append(adicionar)
         elif r1 == '2':
+            os.system('clear')
             delete = input("Qual o número do item que você deseja remover? (Apenas número, por favor)").isdigit()
             del lista_compras[delete]
             continue
         elif r1 == '3':
+            os.system('clear')
             print("\n" + "=" * 30)
-            print("     🛒 LISTA DE COMPRAS")
+            print(f"     {nome_lista}")
             print("=" * 30)
         
-            if not lista_compras:
-                print("Sua lista está vazia.")
-            else:
+            if lista_compras:
                 for i, item in enumerate(lista_compras, start=1):
                     print(f"{i:02d} | {item.capitalize()}")
                     print("=" * 30 + "\n")
+            else:
+                print("\nSua lista está vazia.\n")
+        elif r1 == '4':
+            print(f"Nome atual da sua lista: {nome_lista}")
+            nome_lista = input("Digite o nome para substituir: ")
         else:
             break
     else:
         print("Escolha apenas uma das opções!")
         continue
+print("\nAté mais!")
+        print("Escolha apenas uma das opções!")
+        continue
+print("\nAté mais!")
